cce177a503089c6d1a24d34da9f42334141cffb9
Calvo <joeytheyoung13@gmail.com>
2024-12-16 08:17:34 -0300

    ...

---
+++ lib/domain/listarmedicos.dart
@@ -0,0 +1,78 @@
+import 'package:flutter/material.dart';
+import 'package:medisoft/db/database_helper.dart';
+import 'cadastro_medico.dart';
+
+
+class ListarMedicos extends StatefulWidget {
+  @override
+  _ListarMedicosState createState() => _ListarMedicosState();
+}
+
+
+class _ListarMedicosState extends State<ListarMedicos> {
+  late Future<List<Map<String, dynamic>>> medicos;
+
+
+  @override
+  void initState() {
+    super.initState();
+    medicos = DatabaseHelper.instance.listarMedicos();
+  }
+
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Hospital Regional'),
+        actions: [
+          IconButton(
+            icon: Icon(Icons.person_add_alt_1, color: Color(0xFF44A4D7),),
+            onPressed: () async {
+              await Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) => CadastroMedico()),
+              );
+              setState(() {
+                medicos = DatabaseHelper.instance.listarMedicos();
+              });
+            },
+          ),
+        ],
+      ),
+      body: FutureBuilder<List<Map<String, dynamic>>>(
+        future: medicos,
+        builder: (context, snapshot) {
+          if (!snapshot.hasData) {
+            return Center(child: CircularProgressIndicator());
+          }
+          if (snapshot.data!.isEmpty) {
+            return Center(child: Text('Nenhum médico cadastrado.'));
+          }
+          return GridView.builder(
+            padding: EdgeInsets.all(16),
+            gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+              crossAxisCount: 2,
+              mainAxisSpacing: 10,
+              crossAxisSpacing: 10,
+            ),
+            itemCount: snapshot.data!.length,
+            itemBuilder: (context, index) {
+              final medico = snapshot.data![index];
+              return Card(
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Icon(Icons.person, size: 50, color: Color(0xFF44A4D7)),
+                    Text(medico['nome'], style: TextStyle(fontWeight: FontWeight.bold)),
+                    Text(medico['especialidade']),
+                  ],
+                ),
+              );
+            },
+          );
+        },
+      ),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,19 @@
 import 'package:flutter/material.dart';
-import 'package:myapp/menu_page.dart';
+import '../domain/listarmedicos.dart';
+
 
 void main() {
-  runApp(
-    MaterialApp(
-      home:MenuPage()   ),
-  );
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Hospital Regional',
+      theme: ThemeData(primarySwatch: Colors.blue),
+      home:ListarMedicos(),
+    );
+  }
 }
