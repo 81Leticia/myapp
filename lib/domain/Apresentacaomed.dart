af87090a5c7ce00ca1fe44e5a9e30b52946125ac
Mlarangeiraa <msl20@aluno.ifal.edu.br>
2024-12-21 17:19:38 -0300

    Apresentacaomed.dart

--- lib/domain/Apresentacaomed.dart
+++ lib/domain/Apresentacaomed.dart
@@ -2,46 +2,38 @@ import 'package:flutter/material.dart';
 import 'package:myapp/domain/medico.dart';
 
 class Apresentacaomed extends StatelessWidget {
-  final Medico medico;  // Recebe o objeto Medico
+  final Medico medico;  
 
-  // Construtor da classe, aceitando o objeto Medico
   Apresentacaomed({required this.medico});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text(medico.nome),  // Exibe o nome do médico
+        title: Text(medico.nome), 
       ),
       body: Padding(
         padding: const EdgeInsets.all(16.0),
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
-            // Nome do Médico
             Text('Nome: ${medico.nome}', style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold)),
             SizedBox(height: 10),
 
-            // Especialidade do Médico
             Text('Especialidade: ${medico.especialidade}', style: TextStyle(fontSize: 16)),
             SizedBox(height: 10),
 
-            // CRM do Médico
             Text('CRM: ${medico.crm}', style: TextStyle(fontSize: 16)),
             SizedBox(height: 10),
 
-            // E-mail do Médico
             Text('E-mail: ${medico.email}', style: TextStyle(fontSize: 16)),
             SizedBox(height: 10),
 
-            // Telefone do Médico
             Text('Telefone: ${medico.telefone}', style: TextStyle(fontSize: 16)),
             SizedBox(height: 10),
 
-            // Botão para entrar em contato
             ElevatedButton(
               onPressed: () {
-                // Aqui você pode adicionar a funcionalidade para entrar em contato com o médico
                 ScaffoldMessenger.of(context).showSnackBar(
                   SnackBar(content: Text('Entrando em contato com o médico...')),
                 );
